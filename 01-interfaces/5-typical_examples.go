37442db3af9eeed1bfdea32c3cd8eb9c0f4c3b16
Dmitriy Titov <dtsp@yandex.ru>
2025-04-13 15:51:55 +0300

    Ifaces Examples

--- 01-interfaces/5-typical_examples.go
+++ 01-interfaces/5-typical_examples.go
@@ -1,9 +1,64 @@
 package interfaces
 
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/rs/zerolog/log"
+)
+
 // Задача на web crawler
+//
+// Используя тип данных Parser, нужно написать алгоритм обхода веб-страниц
+// с уччётом гиперссылок и парсинга ссылок с этих страниц.
+// В результате обхода должен быть сформирован массив со всеми найденными ссылками.
+type Parser interface {
+	Parse(url string) []string
+}
 
 // Пакет для модульных тестов логики, зависящей от БД
+type DB interface {
+	GetUser(id int) (*User, error)
+	AddUser(user *User) (*User, error)
+}
 
-// Пакет для надежного выполнения задач
+type User struct {
+	ID    int
+	Email string
+}
+
+type MemDB struct {
+	users  map[int]*User
+	nextID int
+}
+
+func (m *MemDB) GetUser(id int) (*User, error) {
+	return m.users[id], nil
+}
+
+func (m *MemDB) AddUser(user *User) (*User, error) {
+	user.ID = m.nextID
+	m.users[user.ID] = user
+	m.nextID++
+	return user, nil
+}
 
 // Интерфейсы стандартной библиотеки
+func stdLib() {
+	var a any
+
+	switch val := a.(type) {
+	case io.Reader:
+		val.Read(nil)
+	case io.Writer:
+		val.Write(nil)
+	case io.Closer:
+		val.Close()
+	case json.Marshaler:
+		val.MarshalJSON()
+	case json.Unmarshaler:
+		val.UnmarshalJSON(nil)
+	default:
+		log.Info().Msg("var a is none of the known types")
+	}
+}
